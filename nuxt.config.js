1c6946576d27a424d9a96d08852aa422abefc624
Negorizont <boltuev01@mail.ru>
2020-03-14 23:21:25 +0400

    add pages; add scss structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@/node_modules/normalize.css', '@/assets/scss/main.scss'],
   /*
    ** Plugins to load before mounting the App
    */
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <ul class="post-list">
+      <li v-for="post of 100" :key="post" class="post">
+        <h3 class="post__title">User {{ post }}</h3>
+        <p class="post__description">Hello world</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss">
+@import '@/assets/scss/main';
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.post-list {
+  width: 100%;
+  height: 100vh;
+  list-style: none;
+  @include flex(column, space-around, center);
+
+  .post {
+    padding: 10px;
+    border: 1px solid black;
+    background-color: red;
+    margin-top: 20px;
+  }
+}
+</style>
